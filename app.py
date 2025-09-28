66daa1982645accdac05560519c53ca2792069ac
sayemSS <sayembiswas8333@gmail.com>
2025-09-28 12:21:56 +0600

    2.5 flash

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if not api_key:
 
 
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-2.5-flash')
 
 
 chat = model.start_chat(history=[])
